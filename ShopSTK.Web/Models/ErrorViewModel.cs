c634f35c9c887cfa4ef53dea21900a092da714a6
Fellipe Strombeck <fellipe.strombeck@gmail.com>
2025-01-07 19:12:28 -0300

    Criado projetos necessários na solution

---
+++ ShopSTK.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopSTK.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
